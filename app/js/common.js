27fff6afd19115cfa5e13fb342b7956ca91b842e
dpodbereznyh <dpodbereznyh@gmail.com>
2020-05-08 15:02:41 +0500

    ЖК УНИКОМ

---
+++ app/js/common.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+//Переключатель фонового изображения
+$('.toggle').click(function () {
+    if ($('#toggle__input').is(':checked')) {
+        $('.bg__dark').css('opacity', '1');
+        $('.bg__light').css('opacity', '0');
+        $('.toggle__sun').css('opacity', '0.5');
+        $('.toggle__moon').css('opacity', '1');
+    } else {
+        $('.bg__dark').css('opacity', '0');
+        $('.bg__light').css('opacity', '1');
+        $('.toggle__sun').css('opacity', '1');
+        $('.toggle__moon').css('opacity', '0.5');
+    }
+});
+// SmartMenus init
+$(function() {
+    $('#main-menu').smartmenus({
+        mainMenuSubOffsetX: -1,
+        mainMenuSubOffsetY: 4,
+        subMenusSubOffsetX: 6,
+        subMenusSubOffsetY: -6
+    });
+});
+
+// SmartMenus mobile menu toggle button
+$(function() {
+    var $mainMenuState = $('#main-menu-state');
+    if ($mainMenuState.length) {
+        // animate mobile menu
+        $mainMenuState.change(function(e) {
+            var $menu = $('#main-menu');
+            if (this.checked) {
+                $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+            } else {
+                $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+            }
+        });
+        // hide mobile menu beforeunload
+        $(window).bind('beforeunload unload', function() {
+            if ($mainMenuState[0].checked) {
+                $mainMenuState[0].click();
+            }
+        });
+    }
+});
+
+//News Swiper Slider
+    const newsSlider = document.querySelector('.news__slider_container');
+    var myNewsSlider = new Swiper ('.news__slider_container', {
+        slideClass: 'news__slide',
+        wrapperClass: 'news__slider_wrapper',
+        slidesPerView: 1,
+        spaceBetween: 10,
+        lazy: {
+            loadPrevNext: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            440: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            960: {
+                slidesPerView: 4,
+                spaceBetween: 32
+            }
+        }
+    })
+});
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var syntax        = 'scss'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+    gutil         = require('gulp-util' ),
+    sass          = require('gulp-sass'),
+    browserSync   = require('browser-sync'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    cleancss      = require('gulp-clean-css'),
+    rename        = require('gulp-rename'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    notify        = require('gulp-notify'),
+    rsync         = require('gulp-rsync');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false,
+        // open: false,
+        // online: false, // Work Offline Without Internet Connection
+        // ghostMode: false //Disable clicks,scrolls & form inpust on any devices
+        // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+    })
+});
+
+gulp.task('styles', function() {
+    return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+        .pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+        .pipe(rename({ suffix: '.min', prefix : '' }))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        // Without JQuery dependensies
+        // JQuery dependent
+        'app/js/vendors/jquery-3.4.1.js',
+        'app/js/vendors/jquery.smartmenus.js',
+        'app/js/vendors/swiper.min.js',
+        'app/js/vendors/jquery.fancybox.min.js',
+        'app/js/common.js', // Always at the end
+    ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+//gulp.task('rsync', function() {
+//	return gulp.src('app/**')
+//	.pipe(rsync({
+//		root: 'app/',
+//		hostname: 'username@yousite.com',
+//		destination: 'yousite/public_html/',
+//		// include: ['*.htaccess'], // Includes files to deploy
+//		exclude: ['*Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+//		recursive: true,
+//		archive: true,
+//		silent: false,
+//		compress: true
+//	}))
+//});
+
+gulp.task('watch', function() {
+    gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
+    gulp.watch(['app/libs/**/*.js', 'app/js/common.js', 'app/js/vendors/**/*.js'], gulp.parallel('scripts'));
+    gulp.watch('app/*.html', gulp.parallel('code'))
+});
+gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));
